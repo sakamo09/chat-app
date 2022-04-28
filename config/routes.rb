d35368aa937ef270eb0a4817cc3613f283fe00cd
sakamo09 <teamilkyui326@gmail.com>
2022-04-28 14:18:33 +0900

    room_usersテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
-  resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  belongs_to :room
+  belongs_to :user
 end
